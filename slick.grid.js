6b4b9ec224fe780e579e7b186335255888c0de56
Kirk Swenson <kswenson@kcptech.com>
2013-03-05 19:27:18 -0800

    Add onCanvasWidthChanged event and trigger it during column resize so clients can implement live-resize behavior.

--- slick.grid.js
+++ slick.grid.js
@@ -402,6 +402,7 @@ if (typeof Slick === "undefined") {
         $headerRow.width(canvasWidth);
         $headers.width(getHeadersWidth());
         viewportHasHScroll = (canvasWidth > viewportW - scrollbarDimensions.width);
+        trigger(self.onCanvasWidthChanged, { width: canvasWidth });
       }
 
       $headerRowSpacer.width(canvasWidth + (viewportHasVScroll ? scrollbarDimensions.width : 0));
@@ -842,6 +843,7 @@ if (typeof Slick === "undefined") {
               applyColumnHeaderWidths();
               if (options.syncColumnCellResize) {
                 applyColumnWidths();
+                updateCanvasWidth(true);
               }
             })
             .bind("dragend", function (e, dd) {
@@ -3188,6 +3190,7 @@ if (typeof Slick === "undefined") {
       "onKeyDown": new Slick.Event(),
       "onAddNewRow": new Slick.Event(),
       "onValidationError": new Slick.Event(),
+      "onCanvasWidthChanged": new Slick.Event(),
       "onViewportChanged": new Slick.Event(),
       "onColumnsReordered": new Slick.Event(),
       "onColumnsResized": new Slick.Event(),
